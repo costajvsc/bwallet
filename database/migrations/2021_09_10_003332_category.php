987a4299d5bab78a766b41c66c750821cbd00f89
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-09-09 22:30:41 -0300

    feat: Define category migration

---
+++ database/migrations/2021_09_10_003332_category.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Category extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table){
+            $table->bigIncrements('id_category');
+            $table->string('title', 50);
+            $table->string('color', 6)->nullable();
+            $table->string('icon', 15)->nullable();
+            $table->foreignId('id_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
