59e7e1e01d8cc1d5e1de6e7d217ba5f89624001c
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-10-16 12:03:47 -0300

    feat: create _layout subview

---
+++ resources/views/_layout.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dwallet | Carteira digital - @yield('title')</title>
+</head>
+<body>
+    <div class="container">
+        @yield('body')
+    </div>
+</body>
+
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/bootstrap.js')}}"></script>
+
+</html>
