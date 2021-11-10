6a020b175452680cd377fbf7696361fe9446dcae
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-11-10 09:38:02 -0300

    feat: Improve blade layout architecture

--- resources/views/layout/_layout.blade.php
+++ resources/views/layout/_layout.blade.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/tables.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dwallet | Carteira digital - @yield('title')</title>
+    <script src="https://kit.fontawesome.com/d5e0d5b45e.js" crossorigin="anonymous"></script>
+    <title>Dwallet | @yield('title')</title>
 </head>
 <body>
     <div class="container">
+        <div class="my-2">
+            @include('layout/message')
+        </div>
         @yield('body')
     </div>
 </body>
